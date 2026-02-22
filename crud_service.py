1764dd9c90565dfc090482866f019f66b35dd083
tel-kheir <165869766+tel-kheir@users.noreply.github.com>
2026-02-22 22:15:22 +0200

    skeleton

---
+++ crud_service.py
@@ -0,0 +1,28 @@
+import json
+
+
+class CRUDService:
+    def __init__(self, file="data.json"):
+        self.file = file
+
+    def create_resource(self):
+        """
+        TODO: Implement create method
+        """
+        pass
+
+    def update_resource(self):
+        """
+        TODO: Implement update method
+        """
+        pass
+
+    def delete_resource(self):
+        """
+        TODO: Implement delete method
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
